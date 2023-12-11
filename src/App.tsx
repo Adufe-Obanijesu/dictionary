0d5bb0a9422ef0ce6018f7e1f23195670351795b
Adufe Obanijesu <obanijesuadufe8@gmail.com>
2023-12-11 19:50:25 +0000

    Implemented the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 import Navbar from "./components/Navbar";
 
 const App = () => {
 
+  // Mode true is dark mode while false is light mode
+  const [ mode, setMode ] = useState(false);
+  const [ font, setFont ] = useState("sans");
+
   return (
-    <main className="flex justify-center py-4">
+    <main className={`flex justify-center py-4 min-h-screen ${mode && "bg-slate-900 text-slate-300"} font-${font}`}>
       <div className="w-1/2">
-        <Navbar />
+        <Navbar mode={mode} setMode={setMode} setFont={setFont} />
+        dsd
       </div>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import { RiBook2Line } from "react-icons/ri";
-import { FiMoon } from "react-icons/fi";
+import { FiMoon, FiSun } from "react-icons/fi";
 
-const Navbar = () => {
+type props = {
+    mode: boolean
+    setMode: (arg: boolean) => void
+    setFont: (arg: string) => void
+}
+
+const Navbar = ({ mode, setMode, setFont }: props) => {
     return (
         <nav>
             <div className="flex justify-between items-center py-2">
@@ -9,17 +15,23 @@ const Navbar = () => {
                     <RiBook2Line className="text-gray-400 text-4xl" />
                 </div>
                 <div className="flex items-center gap-4">
-                    <select>
-                        <option value="Serif">Serif</option>
-                        <option value="Jost">Jost</option>
-                        <option value="Poppins">Poppins</option>
+                    <select className="focus:outline-none cursor-pointer bg-transparent" onChange={e => setFont(e.target.value)}>
+                        <option className={`${mode && "bg-slate-800"}`} value="sans">Sans</option>
+                        <option className={`${mode && "bg-slate-800"}`} value="serif">Serif</option>
+                        <option className={`${mode && "bg-slate-800"}`} value="mono">Mono</option>
                     </select>
 
-                    <div className="bg-gray-700 p-1 rounded-full w-10 h-6 flex items-center cursor-pointer">
-                        <div className="h-4 w-4 bg-white rounded-full"></div>
+                    <div className="flex gap-2 items-center">
+                        <div className="bg-gray-700 p-1 rounded-full w-10 h-6 flex items-center cursor-pointer" onClick={() => setMode(!mode)}>
+                            <div className={`h-4 w-4 bg-slate-50 rounded-full transition ease-in duration-100 ${mode && "translate-x-4"}`}></div>
+                        </div>
+
+                        {
+                            mode ? <FiSun className="text-slate-300 text-xl" /> : <FiMoon className="text-slate-600 text-xl" />
+                        }
                     </div>
 
-                    <FiMoon className="text-gray-700 text-xl" />
+                    
                 </div>
             </div>
         </nav>
