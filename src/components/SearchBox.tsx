069d817fed2301b64293350d099e739976037683
Adufe Obanijesu <obanijesuadufe8@gmail.com>
2023-12-13 21:36:03 +0000

    second attempthirdix mobile glitch

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -22,7 +22,9 @@ const SearchBox = ({ response, setWord, setSearch }: props) => {
 
     return (
         <div className="bg-white w-full rounded-md absolute top-12 z-20 left-0 min-h-[100px]">
-
+            {
+                response && response.results.data.join(", ")
+            }
             {
                 response && response.results.data.map((word: string) => (
                     <p key={word} className="text-slate-600 border-b py-4 px-4 cursor-pointer transitionItem hover:bg-slate-200" onClick={() => updateSearch(word)}>
